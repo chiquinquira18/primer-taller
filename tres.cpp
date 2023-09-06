e4475d61f7fdeb2ed2e0ebc2836a9335bf5da307
chiquinquira18 <135726689+chiquinquira18@users.noreply.github.com>
2023-09-06 14:23:09 -0500

    .
    .

---
+++ tres.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    cout<<"suma de 2 numeros \n ingresa el numero 1 \n";
+    
+    int num1= 0  ;
+    int num2 = 0 ;
+    
+    cout<<"ingresa el numero 1\n";
+    cin>>num1;
+    cout<<"ingresa el numero 2\n";
+    cin>>num2;
+    int suma = num1+ num2;
+    cout<<"la suma de los 2 numeros es : "<< suma;
+    return 0;
+}
