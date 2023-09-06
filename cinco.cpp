6ac82d19ad49707490b437556366b140c8577c72
chiquinquira18 <135726689+chiquinquira18@users.noreply.github.com>
2023-09-06 14:30:16 -0500

    .
    .

---
+++ cinco.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    cout<<" numeros del 1 la 100 \n";
+    
+    for(int i = 0 ; i<101; i++){
+        cout<<i<<"\n";
+    }
+    
+    return 0;
+}
