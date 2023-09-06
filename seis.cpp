d6b53ab8a88ddbc5580d35208ed399554596a8c1
chiquinquira18 <135726689+chiquinquira18@users.noreply.github.com>
2023-09-06 14:36:57 -0500

    .
    .

---
+++ seis.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    cout<<" hasta que numero quieres una lista \n";
+    
+    int n = 0 ;
+    
+    cin>>n;
+    
+    for(int i = 0 ; i<=n; i++){
+        cout<<i<<"\n";
+    }
+    
+    return 0;
+}
