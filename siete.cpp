282484da7a187012af82db4a763fc8e70024c7a6
chiquinquira18 <135726689+chiquinquira18@users.noreply.github.com>
2023-09-06 14:38:06 -0500

    .
    .

---
+++ siete.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float libra = 0.0 ; 
+    float resultado = 0.0;
+    cout<<" ingresa el numero de libras \n";
+    cin>> libra ;
+    resultado = libra * 0.454 ;
+    cout<< libra<< " libras son " << resultado <<" kg";
+    return 0;
+}
