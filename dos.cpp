6dc3cb6c7c5774e7d80758f6cd63407665d53bf7
chiquinquira18 <135726689+chiquinquira18@users.noreply.github.com>
2023-09-06 14:17:53 -0500

    .
    .

---
+++ dos.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"ingresa tu nombre para saludarte\n";
+    string nombre = "nombre";
+    cin>>nombre;
+    cout<<"hola "<<nombre<<" buenos dias , como estas ?";
+    return 0;
+}
